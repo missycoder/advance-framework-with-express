f18cd9a3405b892156ba57e9ad5aaa5adfd7fc34
missycoder mylan <noreen.taha@outlook.com>
2024-05-01 18:20:22 +0000

    updated bookshelf index.js

--- models/index.js
+++ models/index.js
@@ -24,7 +24,7 @@ const Product = bookshelf.model('Product', {
     },
     tags:function() {
         return this.belongsToMany('Tag');
-    }
+    } 
 } );
 
 // and model name keep to singular form of the table name
